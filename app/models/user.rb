77e2d7f9bba95e05159e042a84f492aae669fb21
Robert Mattei <bob@mwbi.net>
2013-09-21 08:30:44 -0400

    Finish user sighup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50}
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
   uniqueness: { case_sensitive: false }  # uniqueness can fail - in certain race conditions
   has_secure_password  # this add instance attributes password and confirm_password
   validates :password, length: { minimum: 6}
+  
 end
